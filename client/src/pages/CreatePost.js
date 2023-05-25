2c07a4eef40ec06ec9842e95a1aa1bbbf72f6079
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-25 17:49:05 -0300

    add axios to call the backend

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,11 +1,8 @@
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
-import { useState } from "react";
+import React, { useState, useContext } from "react";
 import { Navigate } from "react-router-dom";
-import Editor from "../Editor";
-import { useContext } from "react";
-import { UserContext } from "../UserContext";
 import axios from "axios";
+import Editor from "../Editor";
+import { UserContext } from "../UserContext";
 
 export default function CreatePost() {
   const { userInfo, setUserInfo } = useContext(UserContext);
@@ -16,23 +13,28 @@ export default function CreatePost() {
   const [redirect, setRedirect] = useState(false);
 
   async function createNewPost(ev) {
+    ev.preventDefault();
+
     const data = new FormData();
     data.set("title", title);
     data.set("summary", summary);
-    data.set("content", content); 
+    data.set("content", content);
     data.set("file", files[0]);
     data.set("profileAvatar", userInfo.profilePicture);
-    ev.preventDefault();
 
     try {
-      const response = await axios.post("https://backend-blog-psi.vercel.app/post", data, {
-        withCredentials: true, // Para incluir las cookies
-      });
+      const response = await axios.post(
+        "https://backend-blog-psi.vercel.app/post",
+        data,
+        {
+          withCredentials: true,
+        }
+      );
       if (response.status === 200) {
         setRedirect(true);
       }
     } catch (error) {
-      console.error(error);
+      // Manejar errores aquí
     }
   }
 
--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,71 +1,85 @@
 import { useEffect, useState } from "react";
 import { Navigate, useParams } from "react-router-dom";
+import axios from "axios";
 import Editor from "../Editor";
 
 export default function EditPost() {
   const { id } = useParams();
-  const [title, setTitle] = useState('');
-  const [summary, setSummary] = useState('');
-  const [content, setContent] = useState('');
-  const [files, setFiles] = useState('');
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+  const [files, setFiles] = useState("");
   const [redirect, setRedirect] = useState(false);
   const [selectedFont, setSelectedFont] = useState("Arial");
+
   useEffect(() => {
-
-    fetch(`https://backend-blog-psi.vercel.app/post/` + id,)
-
+    axios.get(`https://backend-blog-psi.vercel.app/post/${id}`)
       .then(response => {
-        response.json().then(postInfo => {
-          setTitle(postInfo.title);
-          setContent(postInfo.content);
-          setSummary(postInfo.summary);
-        });
+        const postInfo = response.data;
+        setTitle(postInfo.title);
+        setContent(postInfo.content);
+        setSummary(postInfo.summary);
+      })
+      .catch(error => {
+        // Manejar errores aquí
       });
   }, []);
 
-  async function updatePost(ev, err) {
+  async function updatePost(ev) {
     ev.preventDefault();
-    const data = new FormData();
-    data.set('title', title);
-    data.set('summary', summary);
-    data.set('content', content);
-    data.set('id', id);
-    if (files?.[0]) {
-      data.set('file', files?.[0]);
 
+    const data = new FormData();
+    data.set("title", title);
+    data.set("summary", summary);
+    data.set("content", content);
+    data.set("id", id);
+    if (files?.[0]) {
+      data.set("file", files?.[0]);
     }
 
-    const response = await fetch('https://backend-blog-psi.vercel.app/post', {
+    try {
+      const response = await axios.put("https://backend-blog-psi.vercel.app/post", data, {
+        withCredentials: true,
+      });
 
-      method: 'PUT',
-      body: data,
-      credentials: 'include',
-    });
-    if (response.ok) {
-      setRedirect(true);
+      if (response.status === 200) {
+        setRedirect(true);
+      }
+    } catch (error) {
+      // Manejar errores aquí
     }
   }
-  // redirect
+
+  // Redireccionar
   if (redirect) {
-    return <Navigate to={'/post/' + id} />
+    return <Navigate to={"/post/" + id} />;
   }
 
   return (
     <form onSubmit={updatePost}>
-      <input type="title"
-        placeholder={'Title'}
+      <input
+        type="title"
+        placeholder={"Title"}
         value={title}
-        onChange={ev => setTitle(ev.target.value)} />
-      <input type="summary"
-        placeholder={'Summary'}
+        onChange={ev => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
         value={summary}
-        onChange={ev => setSummary(ev.target.value)} />
-      <input type="file"
-        onChange={ev => setFiles(ev.target.files)} />
-      <Editor onChange={setContent} value={content}
-        selectedFont={selectedFont} />
-      <button style={{ marginTop: '5px' }}>Update post</button>
-      <br></br><br></br><br></br><hr></hr>
+        onChange={ev => setSummary(ev.target.value)}
+      />
+      <input type="file" onChange={ev => setFiles(ev.target.files)} />
+      <Editor
+        onChange={setContent}
+        value={content}
+        selectedFont={selectedFont}
+      />
+      <button style={{ marginTop: "5px" }}>Update post</button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <hr></hr>
     </form>
   );
 }
