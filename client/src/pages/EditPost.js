82d8f850ff41b94bb894d7478eaefc1b691544db
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-25 23:37:41 -0300

    add cors front end

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -11,9 +11,7 @@ export default function EditPost() {
   const [redirect, setRedirect] = useState(false);
   const [selectedFont, setSelectedFont] = useState("Arial");
   useEffect(() => {
-
-    fetch(`https://backend-blog-psi.vercel.app/post/` + id,)
-
+    fetch(`https://backend-blog-psi.vercel.app/post/` + id)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
@@ -32,40 +30,52 @@ export default function EditPost() {
     data.set('id', id);
     if (files?.[0]) {
       data.set('file', files?.[0]);
-
     }
 
     const response = await fetch('https://backend-blog-psi.vercel.app/post', {
-
       method: 'PUT',
       body: data,
       credentials: 'include',
+      headers: {
+        'Access-Control-Allow-Origin': 'https://sentidos.vercel.app' // Agrega el encabezado CORS
+      }
     });
+
     if (response.ok) {
       setRedirect(true);
     }
   }
+
   // redirect
   if (redirect) {
-    return <Navigate to={'/post/' + id} />
+    return <Navigate to={'/post/' + id} />;
   }
 
   return (
     <form onSubmit={updatePost}>
-      <input type="title"
+      <input
+        type="title"
         placeholder={'Title'}
         value={title}
-        onChange={ev => setTitle(ev.target.value)} />
-      <input type="summary"
+        onChange={ev => setTitle(ev.target.value)}
+      />
+      <input
+        type="summary"
         placeholder={'Summary'}
         value={summary}
-        onChange={ev => setSummary(ev.target.value)} />
-      <input type="file"
-        onChange={ev => setFiles(ev.target.files)} />
-      <Editor onChange={setContent} value={content}
-        selectedFont={selectedFont} />
+        onChange={ev => setSummary(ev.target.value)}
+      />
+      <input type="file" onChange={ev => setFiles(ev.target.files)} />
+      <Editor
+        onChange={setContent}
+        value={content}
+        selectedFont={selectedFont}
+      />
       <button style={{ marginTop: '5px' }}>Update post</button>
-      <br></br><br></br><br></br><hr></hr>
+      <br />
+      <br />
+      <br />
+      <hr />
     </form>
   );
 }
