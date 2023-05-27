c7fcef9c6a88b0b6e46c7f50552d8ba96353d1f5
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-27 00:42:07 -0300

    delete id

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -34,7 +34,7 @@ export default function EditPost() {
       }
       console.log(id,title,content, summary,data);
      
-      const response = await fetch('https://backend-blog-psi.vercel.app/post/' + id, {
+      const response = await fetch('https://backend-blog-psi.vercel.app/post', {
         method: 'PUT',
         body: data,
         credentials: 'include',
