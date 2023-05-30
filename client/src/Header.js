4bc9b7682b7691ef46004e4f6479ad468649ae43
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-30 15:53:26 -0300

    add the jwtkens

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,31 +1,37 @@
 import { Link } from "react-router-dom";
-import { useContext, useEffect,useState } from "react";
+import { useContext, useEffect } from "react";
 import { Navigate, useParams } from "react-router-dom";
 import { UserContext } from "./UserContext";
 import sentidos from './assets/sentidos.png';
 import user from './assets/user.png';
-
+import Cookies from "js-cookie";
 
 export default function Header() {
-  const { setUserInfo, userInfo } = useContext(UserContext);
+  const { setUserInfo, userInfo, setJWT } = useContext(UserContext);
   useEffect(() => {
-
     fetch(`https://backend-blog-psi.vercel.app/profile`, {
-
       credentials: 'include',
-    }).then(response => {
-      response.json().then(userInfo => {
-        setUserInfo(userInfo);
-        console.log(JSON.stringify(userInfo) +  'aca deberia aparecer los datos SUPUESTAMENTE AL LLAMARLO ES POR QUE ES EL');
-      });
+    })
+    .then(response => {
+      if (response.ok) {
+        const token = response.headers.get("Authorization");
+        setJWT(token);
+        Cookies.set("token", token, { expires: 7 });
+        return response.json();
+      } else {
+        throw new Error("Profile request failed");
+      }
+    })
+    .then(data => {
+      setUserInfo(data);
+    })
+    .catch(error => {
+      console.error(error);
     });
   }, []);
 
- 
   function logout() {
-
     fetch('https://backend-blog-psi.vercel.app/logout', {
-
       credentials: 'include',
       method: 'POST',
     });
@@ -42,9 +48,7 @@ export default function Header() {
       <nav>
         {username && (
           <>
-
             <img className="author-avatar-img" src={`https://backend-blog-psi.vercel.app/` + profilePicture} alt="Profile picture" />
-
             <Link to="/create" className="login">Create new post</Link>
             <a onClick={logout} className="register">Logout ({username})</a>
           </>
@@ -57,7 +61,6 @@ export default function Header() {
           </>
         )}
       </nav>
-    </header>
-    
+    </header>   
   );
 }
