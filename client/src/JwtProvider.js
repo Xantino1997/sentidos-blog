c9ff60a78f5c4164c794a5c8dac1d46abc86c09f
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-31 15:56:54 -0300

    delete the jwt

--- client/src/JwtProvider.js
+++ client/src/JwtProvider.js
@@ -1,14 +1,14 @@
-// JwtContext.js
-import { createContext, useState } from 'react';
+// // JwtContext.js
+// import { createContext, useState } from 'react';
 
-export const JwtContext = createContext();
+// export const JwtContext = createContext();
 
-export const JwtProvider = ({ children }) => {
-  const [jwt, setJwt] = useState(null);
+// export const JwtProvider = ({ children }) => {
+//   const [jwt, setJwt] = useState(null);
 
-  return (
-    <JwtContext.Provider value={{ jwt, setJwt }}>
-      {children}
-    </JwtContext.Provider>
-  );
-};
+//   return (
+//     <JwtContext.Provider value={{ jwt, setJwt }}>
+//       {children}
+//     </JwtContext.Provider>
+//   );
+// };
