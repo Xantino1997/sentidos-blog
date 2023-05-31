1880dfef39758c710d8182fd9abeef9b615b642a
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-05-31 16:23:47 -0300

    delete the jw.provider

--- client/src/App.js
+++ client/src/App.js
@@ -13,17 +13,17 @@ function App() {
 
   return (
     <UserContextProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<IndexPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/create" element={<CreatePost />} />
-            <Route path="/post/:id" element={<PostPage />} />
-            <Route path="/edit/:id" element={<EditPost />} />
-            <Route path="/post" element={<PostNext />} />
-          </Route>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/create" element={<CreatePost />} />
+          <Route path="/post/:id" element={<PostPage />} />
+          <Route path="/edit/:id" element={<EditPost />} />
+          <Route path="/post" element={<PostNext />} />
+        </Route>
+      </Routes>
 
     </UserContextProvider>
   );
