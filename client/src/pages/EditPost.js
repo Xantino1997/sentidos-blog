a94de8a7737daa2ebe17ec82e6f06c77552afa40
Cirene1997 <gjoaquinreynoso@gmail.com>
2023-06-01 00:59:10 -0300

    delete{}

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useContext } from "react";
 import { Navigate, useParams } from "react-router-dom";
 import Editor from "../Editor";
-import { TokenContext } from "../TokenProvider";
+import TokenContext  from "../TokenProvider";
 
 export default function EditPost() {
   const { id } = useParams();
