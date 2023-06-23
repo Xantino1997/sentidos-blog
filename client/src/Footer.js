bea45f9359c28daef4bae2c604b6f637cbfd9a8c
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-06-23 11:33:07 -0300

    Update Footer.js

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
                     <a className="footer-whatsapp" href="https://api.whatsapp.com/send?phone=543462529718&text=Hola%20me%20encontré%20con%20esta%20página%20y%20quería%20recibir%20información%20sobre%20Sentidos" target="_blank"><img className="img-footer-whatsapp" src={wts} alt="WhatsApp" /></a>
                     <a className="footer-instagram" href="https://www.instagram.com" target="_blank"><img className="img-footer-instagram" src={inst} alt="Instagram" /></a>
                     <a className="footer-facebook" href="https://www.facebook.com/SentidosAsociacion/" target="_blank"><img className="img-footer-facebook" src={fb} alt="Facebook" /></a>
-                    <a className="footer-facebook" href="https://www.youtube.com/watch?v=XRYLY4qlipE&t=18s" target="_blank"><img className="img-footer-facebook" src={youtube} alt="Facebook" /></a>
+                    <a className="footer-youtube" href="https://www.youtube.com/watch?v=XRYLY4qlipE&t=18s" target="_blank"><img className="img-footer-youtube" src={youtube} alt="Facebook" /></a>
 
                 </div>
             </div> 
