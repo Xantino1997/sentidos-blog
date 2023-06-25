7b8d4621023ac9d40968435f789b5e1cbf5d1561
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-06-25 01:08:24 -0300

    Create PagesWait.js

---
+++ client/src/pages/PagesWait.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Cargando() {
+  return (
+    <div className="contenedor-difuminado">
+      <h1 className="titulo-animado">Cargando...</h1>
+      <div className="circulo"></div>
+    </div>
+  );
+}
