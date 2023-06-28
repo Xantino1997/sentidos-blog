3d18ae8db57e4d292a995dc04a12874442ba1b9c
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-06-28 13:48:41 -0300

    Update Header.js

--- client/src/Header.js
+++ client/src/Header.js
@@ -8,6 +8,8 @@ export default function Header() {
   const { setUserInfo, userInfo } = useContext(UserContext);
   const [redirect, setRedirect] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [categories, setCategories] = useState(false);
+  const [activeButton, setActiveButton] = useState('');
 
   useEffect(() => {
     fetch(`https://backend-blog-psi.vercel.app/profile`, {
@@ -71,113 +73,139 @@ export default function Header() {
     setIsMenuOpen(true);
   }
 
-  const navLinks = document.querySelectorAll('.nav-link');
-
-  function handleMouseMovement() {
-    if (navLinks) {
-      navLinks.forEach((link) => {
-        link.classList.add('active');
-        alert('Click')
-      });
-      document.removeEventListener('mousemove', handleMouseMovement);
-    }
-  }
-
-
-  // Agregar el evento para el movimiento del mouse
-  document.addEventListener('click', handleMouseMovement);
-
-
   return (
     <header className="header-container">
 
       {username ? (
-
         <>
-          <div className={`conteiner-creating`} >
-            <Link to="/create" className={`nav-link-creating `}>
+          <div className="conteiner-creating">
+            <Link
+              to="/create"
+              className={`nav-link-creating ${activeButton === 'create' ? 'active' : ''}`}
+              onClick={() => setActiveButton('create')}
+            >
               Create new post
             </Link>
-            
-            <Link to="/createadvice" className={`nav-link-creating`}>
+
+            <Link
+              to="/createadvice"
+              className={`nav-link-creating ${activeButton === 'createadvice' ? 'active' : ''}`}
+              onClick={() => setActiveButton('createadvice')}
+            >
               Crear Evento
             </Link>
 
-            <Link to="/" onClick={logout} className={`nav-link-creating `}>
+            <Link
+              to="/"
+              onClick={() => {
+                logout();
+                setActiveButton('logout');
+              }}
+              className={`nav-link-creating ${activeButton === 'logout' ? 'active' : ''}`}
+            >
               Logout ({username})
             </Link>
           </div>
+
           <img
-            className={`author-avatar-img `}
+            className="author-avatar-img"
             src={profilePicture}
             alt="Profile picture"
           />
         </>
       ) : (
         <>
-         <img
-            className={`img-sentidos`}
+          <img
+            className="img-sentidos"
             src={sentidos}
             alt="Sentidos"
           />
         </>
       )}
 
-
       <nav className={`navbar ${isMenuOpen ? 'menu-open' : ''}`}>
 
         {!isMenuOpen && (
-
-          <ul className={`nav-links`}>
-
-
+          <ul className="nav-links">
             <li>
-              <Link to="/" className={`nav-link-inside `}>
+              <Link to="/" className="nav-link-inside">
                 Inicio
               </Link>
             </li>
-
-
             <li>
-              <Link to="/about" className={`nav-link-inside`}>
-                Nosotros
+              <Link to="/about" className="nav-link-inside">
+                Conócenos
               </Link>
             </li>
             <li>
-              <Link to="/donar" className={`nav-link-inside`}>
+              <Link to="/donar" className="nav-link-inside">
                 Donar
               </Link>
             </li>
             <li>
-              <Link to="/events" className={`nav-link-inside`}>
+              <Link to="/events" className="nav-link-inside">
                 Eventos
               </Link>
             </li>
             <li>
-              <Link to="/login" className={`nav-link-inside`}>
+              <Link to="/login" className="nav-link-inside">
                 Login
               </Link>
+            </li>
+            <li>
+              <button
+                className={`nav-link-inside-btn ${activeButton === 'categories' ? 'active' : ''}`}
+                onClick={() => {
+                  setCategories(!categories);
+                  setActiveButton('categories');
+                }}
+              >
+                Categoría
+              </button>
             </li>
           </ul>
         )}
 
+        {categories && (
+          <ul className={`sub-categories ${categories ? 'open' : ''}`}>
+            <li>
+              <Link to="/category/medicina" className="nav-link-inside-sub">
+                Medicina
+              </Link>
+            </li>
+            <li>
+              <Link to="/category/psicologia" className="nav-link-inside-sub">
+                Psicología
+              </Link>
+            </li>
+            <li>
+              <Link to="/category/bullying" className="nav-link-inside-sub">
+                Bullying
+              </Link>
+            </li>
+            <li>
+              <Link to="/category/life" className="nav-link-inside-sub">
+                Vida Cotidiana
+              </Link>
+            </li>
+            <li>
+              <Link to="/category/asesoramiento" className="nav-link-inside-sub">
+                Asesoramiento
+              </Link>
+            </li>
+          </ul>
+        )}
       </nav>
 
-      <>
-
-        <div className="menu-toggle" onClick={toggleMenu}>
-          {isMenuOpen ? (
-            <>&#9776;</>
-
-          ) : (
-            <div className="close-icon" onClick={closeMenu}>
-              X
-            </div>
-          )}
-        </div>
-      </>
+      <div className="menu-toggle" onClick={toggleMenu}>
+        {isMenuOpen ? (
+          <>&#9776;</>
+        ) : (
+          <div className="close-icon" onClick={closeMenu}>
+            X
+          </div>
+        )}
+      </div>
     </header>
-
-
   );
 }
