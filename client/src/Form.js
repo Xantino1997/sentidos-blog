0af41ac88a88db8fc3b727eda9271681a5122629
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-06-28 22:05:48 -0300

    Update Form.js

--- client/src/Form.js
+++ client/src/Form.js
@@ -1,71 +1,106 @@
 import { useState } from "react";
 import Swal from "sweetalert2";
+import { Link } from "react-router-dom";
+
 
 export default function Form() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [showTermsAlert, setShowTermsAlert] = useState(false);
+  const [termsAccepted, setTermsAccepted] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    // Crear objeto con los datos del formulario
-    const formData = {
-      name,
-      email,
-    };
+    // Verificar si se mostró la alerta de términos y condiciones
+    if (showTermsAlert) {
+      // Verificar si se aceptaron los términos y condiciones
+      if (termsAccepted) {
+        // Crear objeto con los datos del formulario, incluyendo el párrafo
+        const formData = {
+          name,
+          email,
+          terms: `Yo ${name} acepto los términos y condiciones y soy consciente de lo que mencionan los términos y condiciones de Sentidos. Estoy plenamente consciente de lo expuesto en ellos, los leí y los entendí.`,
+        };
 
-    // ${process.env.REACT_APP_API_URL}
-    // Enviar datos a la base de datos utilizando fetch
-    fetch(`https://backend-blog-psi.vercel.app/suscriptors`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(formData)
-    })
-      .then((response) => {
-        if (response.ok) {
-          // Mostrar ventana emergente de éxito
-          Swal.fire({
-            icon: "success",
-            title: "¡Gracias por suscribirte a nuestro post!",
-            showConfirmButton: false,
-            timer: 2000
-          }).then(() => {
-            // Limpiar los campos name y email
-            setName("");
-            setEmail("");
+        // Enviar datos a la base de datos utilizando fetch
+        fetch(`https://backend-blog-psi.vercel.app/suscriptors`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(formData)
+        })
+          .then((response) => {
+            if (response.ok) {
+              // Mostrar ventana emergente de éxito
+              Swal.fire({
+                icon: "success",
+                title: "¡Gracias por suscribirte a nuestro post!",
+                showConfirmButton: false,
+                timer: 2000
+              }).then(() => {
+                // Limpiar los campos name y email
+                setName("");
+                setEmail("");
+                setTermsAccepted(false);
+              });
+            } else {
+              // Mostrar ventana emergente de error
+              Swal.fire({
+                icon: "error",
+                title: "Oops... Ocurrió un error",
+                text: "Parece que el correo ya existe, intenta otro por favor."
+              });
+            }
+          })
+          .catch((error) => {
+            // Mostrar ventana emergente de error
+            Swal.fire({
+              icon: "error",
+              title: "Oops... Ha ocurrido un error",
+              text: "Por favor, inténtalo de nuevo más tarde."
+            });
           });
-        } else {
-          // Mostrar ventana emergente de error
-          Swal.fire({
-            icon: "error",
-            title: "Oops... Ocurrio un error",
-            text: "Parece que el correo ya existe, intenta otro por favor."
-          });
-        }
-      })
-      .catch((error) => {
-        // Mostrar ventana emergente de error
+      } else {
+        // Mostrar ventana emergente de error si no se aceptaron los términos y condiciones
         Swal.fire({
           icon: "error",
-          title: "Oops... Ha ocurrido un error",
-          text: "Por favor, inténtalo de nuevo más tarde."
+          title: "Oops... Ocurrió un error",
+          text: "Debes aceptar los términos y condiciones para continuar."
         });
+      }
+    } else {
+      // Mostrar ventana emergente de términos y condiciones
+      Swal.fire({
+        title: "Antes de continuar",
+        text: "Queremos que leas y aceptes nuestros términos y condiciones.",
+        showCancelButton: true,
+        confirmButtonText: "Aceptar",
+        cancelButtonText: "Cancelar",
+        reverseButtons: true
+      }).then((result) => {
+        if (result.isConfirmed) {
+          setShowTermsAlert(true);
+        }
       });
-
-      setName("");
-      setEmail("");
+    }
   };
 
   return (
     <div className="conteiner-form">
+
       <form className="footer-form" onSubmit={handleSubmit}>
+        <div className="unsuscribe">
+          <Link to="/desuscribir" style={{ fontSize: "15px", color: "#fff", textDecoration: "underline" }}>
+            Dejar de ser suscriptor
+          </Link>
+        </div>
         <br />
         <br />
         <hr />
         <hr />
-        <h3 className="titulo-form">Suscribe</h3>
+        <h3 className="titulo-form">Suscríbete</h3>
         <input
           type="text"
           placeholder="Nombre"
@@ -78,13 +113,27 @@ export default function Form() {
           value={email}
           onChange={(event) => setEmail(event.target.value)}
         />
+
         <button className="btn-form-suscribe" type="submit">Enviar</button>
         <br />
         <br />
-        <hr />
         <br />
         <hr />
+        <br />
+
+        <div className="terminos">
+          <input
+            type="checkbox"
+            checked={termsAccepted}
+            onChange={(event) => setTermsAccepted(event.target.checked)}
+            style={{ fontSize: "10px", color: "#fff", textDecoration: "underline" }}
+          />
+          <Link to="/term" style={{ fontSize: "15px", color: "#fff", textDecoration: "underline" }}>
+            Acepto los términos y condiciones
+          </Link>
+        </div>
       </form>
+
     </div>
   );
 }
