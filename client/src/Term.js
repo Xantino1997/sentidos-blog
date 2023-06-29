0b481930bcef96f3575c63716c35d8d39756045a
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-06-28 22:07:06 -0300

    Create Term.js

---
+++ client/src/Term.js
@@ -0,0 +1,40 @@
+export default function Term() {
+    return (
+        <div style={{ textAlign: "center", border: "solid 2px red",padding:"25px",margin:"25px",marginBottom:"45px" }}>
+            <h3>Términos y Condiciones de Sentidos</h3>
+            <p>
+
+                Bienvenido/a a Sentidos. Antes de suscribirte a nuestro servicio y proporcionar tus datos personales, te pedimos que leas detenidamente los siguientes términos y condiciones. Estos términos establecen los derechos y responsabilidades tanto del suscriptor (tú) como de Sentidos en relación con el uso de tus datos personales.
+                <br></br>
+                <br></br>
+                <h4>Aceptación de los Términos y Condiciones:</h4>
+                Al suscribirte a Sentidos, aceptas plenamente estos términos y condiciones y te comprometes a cumplir con ellos. Si no estás de acuerdo con alguno de los términos establecidos, te recomendamos que no te suscribas a nuestro servicio.
+                <br></br>
+                <br></br>
+                <h4>Uso de los Datos Personales:</h4>
+                Como parte de tu suscripción a Sentidos, recopilaremos y utilizaremos ciertos datos personales, como tu nombre y correo electrónico. Estos datos serán utilizados de manera no comercial para enviarte información relacionada con las actividades y labores de Sentidos, incluyendo noticias, actualizaciones, eventos y otros contenidos relevantes.
+                <br></br>
+                <br></br>
+                <h4>Propósito y Confidencialidad de los Datos:</h4>
+                Entendemos la importancia de la privacidad y nos comprometemos a tratar tus datos personales de forma confidencial y segura. Utilizaremos tus datos exclusivamente para los fines mencionados anteriormente y no los compartiremos con terceros sin tu consentimiento expreso, a menos que así lo requiera la ley.
+                <br></br>
+                <br></br>
+                <h4>Cesión de Derechos:</h4>
+                Al aceptar estos términos y condiciones, otorgas a Sentidos el derecho de utilizar y procesar tus datos personales según lo establecido en el punto 2. Esta cesión de derechos implica que renuncias a cualquier reclamación o compensación económica por parte de Sentidos debido al uso no comercial de tus datos.
+                <br></br>
+                <br></br>
+               <h4> Cancelación de la Suscripción:</h4>
+                Si en algún momento deseas cancelar tu suscripción a Sentidos y dejar de recibir comunicaciones de nuestra parte, podrás hacerlo siguiendo las instrucciones de cancelación que proporcionamos en cada correo electrónico o poniéndote en contacto con nosotros directamente. En este caso, dejaremos de utilizar tus datos personales para enviarte información.
+                <br></br>
+                <br></br>
+                <h4>Cambios en los Términos y Condiciones:</h4>
+                Sentidos se reserva el derecho de modificar estos términos y condiciones en cualquier momento y sin previo aviso. Te recomendamos que revises periódicamente esta página para mantenerte informado/a sobre cualquier actualización. El uso continuado de nuestros servicios después de cualquier modificación implica tu aceptación de los nuevos términos y condiciones.
+                <br></br>
+                <br></br>
+                <h4>Ley Aplicable:</h4>
+                Estos términos y condiciones se rigen por las leyes vigentes del país donde opera Sentidos. En caso de cualquier controversia o disputa relacionada con estos términos, las partes se someterán a la jurisdicción exclusiva de los tribunales competentes en dicho país.
+
+                Si tienes alguna pregunta o inquietud relacionada con estos términos y condiciones, te invitamos a ponerte en contacto con nosotros. Agradecemos tu confianza al suscribirte a Sentidos y nos comprometemos a proteger tus datos personales de acuerdo con estos términos y condiciones.</p>
+        </div>
+    )
+}
