d4c91219fed2d4d995ad11af0fd7e0200461e853
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2023-07-16 22:52:36 -0300

    Update Events.js

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,21 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { isAfter, parseISO, formatDistance, differenceInDays } from "date-fns";
 
 export default function Events() {
   const [events, setEvents] = useState([]);
+  const eventRef = useRef(null);
 
   useEffect(() => {
-    // Obtener los eventos desde el backend
     fetch("https://backend-blog-psi.vercel.app/getadvice")
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setEvents(data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error al obtener los eventos:", error);
-        // Manejar el error de alguna forma (mostrar mensaje de error, etc.)
       });
   }, []);
+
+  const handleShareLink = (event) => {
+    const eventIndex = parseInt(event.target.getAttribute("data-index"));
+    const eventToShare = events[eventIndex];
+
+    const shareURL = `https://sentidos.vercel.app/events`;
+    const selectedNetwork = event.target.getAttribute("data-network");
+
+    if (selectedNetwork === "facebook") {
+      shareOnFacebook(shareURL);
+    }
+
+    if (selectedNetwork === "whatsapp") {
+      shareOnWhatsApp(shareURL);
+    }
+
+    if (selectedNetwork === "twitter") {
+      shareOnTwitter(shareURL);
+    }
+  };
+
+  const shareOnFacebook = (url) => {
+    const facebookURL = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(url)}`;
+    window.open(facebookURL, "_blank");
+  };
+
+  const shareOnWhatsApp = (url) => {
+    const whatsappURL = `https://web.whatsapp.com/send?text=${encodeURIComponent(url)}`;
+    window.open(whatsappURL, "_blank");
+  };
+
+  const shareOnTwitter = (url) => {
+    const twitterURL = `https://twitter.com/intent/tweet?url=${encodeURIComponent(url)}`;
+    window.open(twitterURL, "_blank");
+  };
 
   return (
     <>
@@ -29,25 +63,43 @@ export default function Events() {
           {events.map((event, index) => {
             const eventDate = parseISO(event.eventDate);
             const currentDate = new Date();
-            const distance = isAfter(currentDate, eventDate)
-              ? <b>Evento ya pasado</b>
-              : formatDistance(currentDate, eventDate, { addSuffix: true });
-
+            const distance = isAfter(currentDate, eventDate) ? (
+              <b>Evento ya pasado</b>
+            ) : (
+              formatDistance(currentDate, eventDate, { addSuffix: true })
+            );
             const daysRemaining = differenceInDays(eventDate, currentDate);
 
             return (
-              <div className="event" key={index}>
-              
-              <div className="image-container">
+              <div className="event" key={index} ref={eventRef}>
+                <div className="image-container">
                   <img className="event-image" src={event.image} alt="" />
                 </div>
                 <h2 className="title-info-event">{event.title}</h2>
                 <p>{event.description}</p>
-                <p><b>El evento tendrá lugar la fecha: {event.eventDate.split('T')[0]}.</b></p>
+                <p>
+                  <b>El evento tendrá lugar la fecha: {event.eventDate.split("T")[0]}.</b>
+                </p>
                 <p>{distance}</p>
                 {!isAfter(currentDate, eventDate) && (
-                  <p style={{ color: "white" }}><b>Días restantes: {daysRemaining}</b></p>
+                  <p style={{ color: "white" }}>
+                    <b>Días restantes: {daysRemaining}</b>
+                  </p>
                 )}
+                <div className="events-comparte">
+                  <h1>Comparte</h1>
+                  <div className="events-comparte-redes">
+                    <button className="events-comparte-redes-btn" data-index={index} data-network="facebook" onClick={handleShareLink}>
+                      Facebook
+                    </button>
+                    <button className="events-comparte-redes-btn" data-index={index} data-network="whatsapp" onClick={handleShareLink}>
+                      WhatsApp
+                    </button>
+                    <button className="events-comparte-redes-btn" data-index={index} data-network="twitter" onClick={handleShareLink}>
+                      Twitter
+                    </button>
+                  </div>
+                </div>
               </div>
             );
           })}
@@ -60,4 +112,3 @@ export default function Events() {
     </>
   );
 }
-
